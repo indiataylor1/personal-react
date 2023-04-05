bcf7cddcba9a704c84e4d76acf1c8fc5ccf30111
indiataylor1 <taylorindia92@gmail.com>
2023-04-05 16:28:07 +0100

    Updated links to bootstrap in app.js and navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/images/logo.svg";
 import navIcon1 from "..assets/images/nav-icon1.svg";
 import navIcon2 from "..assets/images/nav-icon2.svg";
