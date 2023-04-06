2583e382ae47a9f7c21dda75f9589cbf9402fd8c
indiataylor1 <taylorindia92@gmail.com>
2023-04-06 11:18:12 +0100

    links fixed animate css & reactonscreen installed

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,11 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+import meter1 from "../assets/images/meter-1.svg";
+import meter2 from "../assets/images/meter-2.svg";
+import meter3 from "../assets/images/meter-3.svg";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png";
+import arrow1 from "../assets/images/arrow1.svg";
+import arrow2 from "../assets/images/arrow2.svg";
+import colorSharp from "../assets/images/color-sharp.png";
 
 export const Skills = () => {
   const responsive = {
